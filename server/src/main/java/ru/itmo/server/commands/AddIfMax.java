b7a9e7afc93afa34cca9e246c39d2c21c586099e
Matvei Sologubov <matthew.sologubov@gmail.com>
2025-06-18 13:53:41 +0300

    feat: Implement 'Add If Max' command

--- server/src/main/java/ru/itmo/server/commands/AddIfMax.java
+++ server/src/main/java/ru/itmo/server/commands/AddIfMax.java
@@ -5,9 +5,13 @@ import ru.itmo.common.network.requests.AddIfMaxRequest;
 import ru.itmo.common.network.requests.Request;
 import ru.itmo.common.network.responses.AddIfMaxResponse;
 import ru.itmo.common.network.responses.Response;
+import ru.itmo.server.db.TicketRepository;
 import ru.itmo.server.managers.CollectionManager;
 
-import java.util.Collections;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
 
 /**
  * 'Add If Max' command adds Ticket to collection if its valid and has maximum price
@@ -29,17 +33,26 @@ public class AddIfMax extends Command {
         AddIfMaxRequest addRequest = (AddIfMaxRequest) request;
         Ticket ticket = new Ticket(addRequest.getTicket());
 
-        if (collectionManager.getCollection().isEmpty()) {
-            collectionManager.add(ticket);
-            return new AddIfMaxResponse(true, null);
+        Optional<Ticket> currentMax = collectionManager.getCollection()
+                .stream().max(Ticket::compareTo);
+
+        if (currentMax.isPresent() && ticket.compareTo(currentMax.get()) <= 0) {
+            String message = "Ticket not added to collection. Current max price is " + currentMax.get().getPrice();
+            return new AddIfMaxResponse(false, message);
         }
 
-        float maxPrice = Collections.max(collectionManager.getCollection()).getPrice();
-        if (ticket.getPrice() > maxPrice) {
-            collectionManager.add(ticket);
-            return new AddIfMaxResponse(true, null);
+        try (PreparedStatement statement = TicketRepository.prepareAddStatement(ticket);
+             ResultSet resultSet = statement.executeQuery()){
+
+            if (resultSet.next()) {
+                long generatedId = resultSet.getLong(1);
+                ticket.setId(generatedId);
+                collectionManager.add(ticket);
+            }
+        } catch (SQLException e) {
+            return new AddIfMaxResponse(false, e.getMessage());
         }
 
-        return new AddIfMaxResponse(false, "Ticket not added to collection. Current max price is " + maxPrice);
+        return new AddIfMaxResponse(true, null);
     }
 }
